5fbd60f7535f61fd46f2158138f60ca14af4be6c
briantoanle <68524814+briantoanle@users.noreply.github.com>
2022-11-13 22:12:54 -0500

    fixed some more things

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ def storeWordsIntoList():
 # get a random word from master list
 def getWord():
     secretWord = random.randint(0,len(masterList)-1)
-    #print(masterList[secretWord])
     return masterList[secretWord]
 
 
@@ -36,15 +35,87 @@ def guessing():
     while stillGuessing:
         tempChar = str(input("Please enter the character you want to guess:"))
 
-def hangmanConversion(word, letter):
-  letterPosition = word.find(letter)
+def inputValidation(inputStr):
+    if len(inputStr) > 1:
+        print('Please enter one character only. ')
+        return 0
+    elif inputStr.isdigit():
+        print('Please enter a character. ')
+        return 0
+    else:
+        return inputStr
+def hangmanGraphic(lifeNum):
+    if lifeNum == 0:
+        print(" ")
+        print("______")
+        print("|     |")
+        print("|      ")
+        print("|      ")
+        print("|      ")
+        print("|      ")
 
-  for index in range(len(word)):
-    if word[index] == word[letterPosition]:
-        print(letter, end = " ")
+    elif lifeNum == 1:
+        print(" ")
+        print("______")
+        print("|     |")
+        print("|     0")
+        print("|      ")
+        print("|      ")
+        print("|      ")
+
+    elif lifeNum == 1:
+        print(" ")
+        print("______")
+        print("|     |")
+        print("|     0")
+        print("|      ")
+        print("|      ")
+        print("|      ")
+
+    elif lifeNum == 2:
+        print(" ")
+        print("______")
+        print("|     |")
+        print("|     0")
+        print("|     |")
+        print("|      ")
+        print("|      ")
+
+    elif lifeNum == 3:
+        print(" ")
+        print("______")
+        print("|     |")
+        print("|     0")
+        print("|     |")
+        print("|    / ")
+        print("|      ")
+
+    elif lifeNum == 4:
+        print(" ")
+        print("______")
+        print("|     |")
+        print("|     0")
+        print("|     |")
+        print("|    / \\")
+        print("|      ")
+
+    elif lifeNum == 5:
+        print(" ")
+        print("______")
+        print("|     |")
+        print("|     0")
+        print("|    /|")
+        print("|    / \\")
+        print("|      ")
 
     else:
-        print("_", end= " ")
+        print(" ")
+        print("______")
+        print("|     |")
+        print("|     0")
+        print("|    /|\\")
+        print("|    / \\")
+        print("|      ")
 
 def main():
     # get .txt file and store it into master list
@@ -52,23 +123,35 @@ def main():
 
     # store the secret word inside a variable
     word = getWord()
+
     hangman = ''
     for i in range(len(word)):
         hangman += '-'
-    print(word)
-    print(hangman)
 
+    correct = 0
+    wrong = 0
     stillGuessing = True
     while stillGuessing:
-        tempChar = input('Enter your guess ')
-        letterPos = word.find(tempChar)
-        for i in range(len(word)):
-            if tempChar == word[i]:
-                hangman = hangman[:i] + word[i] + hangman[i+1:]
+        print(word)
+        print(hangman)
+        userInput = input('Enter your guess ')
+        in1 = inputValidation(userInput)
+        if in1 == userInput:
+
+            for i in range(len(word)):
+                if userInput == word[i]:
+                    hangman = hangman[:i] + word[i] + hangman[i+1:]
+                    correct +=1
+                else:
+                    wrong+=1
+                    print('here')
+        print('corr',correct)
         print(hangman)
         if hangman == word:
-            print('you won')
-    print(word)
-    print(hangman)
+            stillGuessing = False
 
-main()
+    print('you won')
+    #print(word)
+    #print(hangman)
+
+#main()
